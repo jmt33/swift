c8c58ad67516d9ad366a18a95e1ba00f3e7c062b
jinzhe <129@jinzhe.net>
2014-06-15 03:47:55 +0800

    array

---
+++ array.swift
@@ -0,0 +1,22 @@
+extension Array{
+    //随机排序
+    func join(s:String)->String{
+        return s.join(self)
+    }
+    //是否在数组内
+    func has(){
+        
+    }
+    func random(){
+        
+    }
+    /*
+        reverse()
+        removeAtIndex(4)
+        removeLast()
+        removeAll()
+    */
+}
+var arr=["1","2","3"]
+
+    arr.join("|")
